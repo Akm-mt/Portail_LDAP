7e81174b0fe22db0b17226ef84d557bacb70bbdb
akm <mattoir976@gmail.com>
2020-11-16 15:46:47 +0100

    yes

---
+++ src/Controller/MainController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+
+class MainController extends AbstractController
+{
+    /**
+     * @Route("/home", name="home_index")
+     */
+    public function login(): Response
+    {
+
+        return $this->render('main/home.html.twig');
+    }
+
+}
