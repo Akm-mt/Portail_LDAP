3cecc6e67c7bd50b9f92ffad5cb84d0da3897440
akm <mattoir976@gmail.com>
2020-11-17 14:00:43 +0100

    comit

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -6,11 +6,16 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+use Symfony\Component\Ldap\Ldap;
+use Symfony\Component\Ldap\Adapter\QueryInterface;
+
+
 
 class MainController extends AbstractController
 {
     /**
      * @Route("/home", name="home_index")
+     * @Route("/", name="home")
      */
     public function login(): Response
     {
@@ -18,4 +23,24 @@ class MainController extends AbstractController
         return $this->render('main/home.html.twig');
     }
 
+    /**
+     * @Route("/manage-users", name="manage_users")
+     */
+    public function manage_user(Ldap $ldap): Response
+    {   
+
+        $ldap = Ldap::create('ext_ldap', [
+            'host' => '192.168.42.77',
+            'port' => '389',
+        ]);
+        $search_dn = "cn=admin,dc=akmt,dc=local";
+        $search_password = "20Am1299";
+        $ldap->bind($search_dn, $search_password);
+        $query = $ldap->query('dc=akmt,dc=local', 'objectClass=inetOrgPerson');
+        $results = $query->execute()->toArray();
+        
+        return $this->render('main/manage_users.html.twig',['results' => $results]);
+    }
+
 }
+
--- webpack.config.js
+++ webpack.config.js
@@ -36,7 +36,7 @@ Encore
      * Each entry will result in one JavaScript file (e.g. app.js)
      * and one CSS file (e.g. app.css) if your JavaScript imports CSS.
      */
-    //.addEntry('app', './assets/app.js')
+    .addEntry('app', './assets/app.js')
     .addEntry('login-page', './assets/login-page.js')
     //.addEntry('page1', './assets/page1.js')
     //.addEntry('page2', './assets/page2.js')
