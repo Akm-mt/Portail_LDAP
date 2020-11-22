5bc5ef5b167e6527f2b56ca0766861991d93da93
akm <mattoir976@gmail.com>
2020-11-22 01:51:24 +0100

    commit

--- assets/manage-users-page.js
+++ assets/manage-users-page.js
@@ -6,19 +6,238 @@
  */
 
 // any CSS you import will output into a single css file (app.css in this case)
-import './styles/manage-users-page.css';
+import './styles/manage-users-page.scss';
 
 // Need jQuery? Install it with "yarn add jquery", then uncomment to import it.
 // import $ from 'jquery';
- //$(document).ready(function (e) {
+//$(document).ready(function (e) {
 
-    $.ajax("http://127.0.0.1:8000/manage-users",   // request url
-    {            
-        success: function (data, status, xhr) {
-                console.log(data);    
+$(document).ready(function () {
+
+    var DT1 = initTable();
+
+    $(".selectall-checkbox").on("click", function (e) {
+        if ($(this).is(":checked")) {
+            DT1.rows().select();
+        } else {
+            DT1.rows().deselect();
         }
     });
 
+    $('#user-table tbody').on('click', '.btn-edit', function () {
+        var data = DT1.row($(this).parents('tr')).data();
+        alert(data[0] + "'s salary is: " + data[6]);
+    });
 
- 
-//});
+    $('#user-table tbody').on('click', '.btn-del', function () {
+        var globalSelected= [];
+        var data = DT1.row($(this).parents('tr')).data();
+        delete data[0];
+        globalSelected.push(Object.assign({},transfomDataWithHeader(data)));
+        deleteUsers(Object.assign({},globalSelected), 1);
+
+    });
+
+});
+function addUser() {
+
+    var url = new URL(window.location.href);
+    url.hash = "";
+    url.search = "";
+
+    $.confirm({
+        icon: 'fa fa-warning',
+        title: 'Confirmation de suppression',
+        content: '' +
+        '<form action="" class="formName">' +
+        '<div class="form-group">' +
+        '<label>Enter something here</label>' +
+        '<input type="text" placeholder="Your name" class="name form-control" required />' +
+        '</div>' +
+        '</form>',
+        animation: 'opacity',
+        theme: 'dark',
+        buttons: {
+            cancel: function () {
+                return;
+            },
+            Ajouter: {
+                btnClass: 'btn-info',
+                action:function () {
+                    $.alert('Your name is ' + name);
+                    // $.ajax({
+                    //     url: url,
+                    //     type: 'DELETE',
+                    //     contentType: "application/json",
+                    //     dataType: "json",
+                    //     data: JSON.stringify(arrayUsers)
+                    // }).always(function (data_request) {
+                    //     toastr.success(data_request['message']);
+                    //     reloadTable();
+                    // });
+                },
+            }
+        }
+    });    
+}
+
+function deleteUsers(arrayUsers, count) {
+
+    var url = new URL(window.location.href);
+    url.hash = "";
+    url.search = "";
+
+    $.confirm({
+        icon: 'fa fa-warning',
+        title: 'Confirmation de suppression',
+        content: 'Voulez vous supprimmer les élements selectionner ('+ count +') ? ',
+        animation: 'opacity',
+        theme: 'dark',
+        buttons: {
+            cancel: function () {
+                return;
+            },
+            confirm: {
+                btnClass: 'btn-danger',
+                action:function () {
+                    $.ajax({
+                        url: url,
+                        type: 'DELETE',
+                        contentType: "application/json",
+                        dataType: "json",
+                        data: JSON.stringify(arrayUsers)
+                    }).always(function (data_request) {
+                        toastr.success(data_request['message']);
+                        reloadTable();
+                    });
+                },
+            }
+        }
+    });    
+}
+
+
+
+function reloadTable(){
+    var url = new URL(window.location.href);
+    url.hash = "";
+    url.search = "";
+
+    $.ajax({
+        url: url,
+        type: 'GET',
+    }).always(function (data_request) {
+        $('#global-content').empty();
+        $('#global-content').append(data_request);
+        $('#user-table').dataTable().fnDestroy();
+        initTable();
+    });
+    
+    return ;
+}
+
+function initTable(){
+
+    var DT1 = $('#user-table').DataTable({
+        "scrollY": "70vh",
+        "scrollCollapse": true,
+        columnDefs: [{
+            orderable: false,
+            className: 'select-checkbox',
+            targets: 0
+        }, {
+            orderable: false,
+            targets: -1,
+            data: null,
+            defaultContent: '<button type="button" class="btn btn-warning btn-edit p-1 mx-1"><i class="fas fa-user-edit"></i></button><button type="button" class="btn btn-danger btn-del px-2 py-1"><i class="fas fa-trash"></i></button>'
+        }
+        ],
+        dom: 'Bfrtip',
+        buttons: [
+            {
+                text: 'Add',
+                className: 'btn-info',
+                action: function () {
+                    toastr.success('Ajouter un utilisateur !');
+                    addUser();
+                }
+            },
+            {
+                text: 'Delete',
+                className: 'btn-danger',
+                action: function () {
+                    var dataRows = getDataRowSelected(DT1);
+                    var count = $.map(dataRows, function(value, index){
+                        return [value];
+                    });
+                    if (count.length > 0){
+                        deleteUsers(dataRows, count.length);
+                    }else{
+                        toastr.error('Vous devez selectionner au moins un élement !');
+                    }
+                }
+            }
+        ],
+        'select': {
+            style: 'multi',
+            selector: 'td:first-child',
+        },
+        'order': [[1, 'asc']],
+        "language": {
+            "url": "//cdn.datatables.net/plug-ins/1.10.21/i18n/French.json"
+        },
+    });
+    return DT1;
+}
+
+function getDataRowSelected(table){
+
+    var globalSelected = [];
+    
+    table.rows({ selected: true }).every(function (rowIdx, tableLoop, rowLoop) {
+        var dataRow = {};
+        var data = $.map(this.data(), function (value, index) {
+            return [value];
+        });
+        data.shift();
+
+        var dataRow = transfomDataWithHeader(data);
+
+        globalSelected.push(Object.assign({}, dataRow));
+    });
+    
+    return Object.assign({},globalSelected);
+}
+
+function transfomDataWithHeader(objectdata){
+    var columnNames = [];
+    var dataRow = [];
+    var data = $.map(objectdata, function(value, index){
+        return [value];
+    });
+    $("#user-table thead tr th").each(function () {
+        columnNames.push($(this).text()); //This executes once per column showing your column names!
+    });
+    columnNames.shift();  // Removes the first element from an array and returns only that element.
+    columnNames.pop();
+    
+    for (var i = 0, len = columnNames.length; i < len; i++) {
+        if(/<\/?[a-z][\s\S]*>/i.test(data[i])){
+            dataRow[columnNames[i]] = transformHtmlToStr(data[i]);
+        }else{
+            dataRow[columnNames[i]] = data[i];
+        }
+    }
+
+    return dataRow ;
+}
+
+function transformHtmlToStr(data){
+    var str1 = "" ;
+    $.each($.parseHTML(data), function(index,value) {
+        if ( /\S/.test(value.textContent) == true ){
+            str1 += value.textContent +';'
+        } 
+    });
+    return str1.slice(0, -1);
+}
--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -18,57 +18,57 @@ class MainController extends AbstractController
 {
 
     public $array_ldap = array(
-        'buildingname'=>'Building',
-        'c'=>'Country',
-        'cn'=>'Full Name',
-        'co'=>'Country',
-        'comment'=>'Comment',
-        'commonname'=>'Full Name',
-        'company'=>'Company',
-        'description'=>'Description',
-        'distinguishedname'=>'Distinguished Name',
-        'dn'=>'Distinguished Name',
-        'department'=>'Department',
-        'displayname'=>'Full Name',
-        'facsimiletelephonenumber'=>'Facsimile',
-        'fax'=>'Facsimile',
-        'friendlycountryname'=>'Country',
-        'givenname'=>'First Name',
-        'homephone'=>'Home Telephone',
-        'homepostaladdress'=>'Home Address',
-        'info'=>'Information',
-        'initials'=>'Middle Initial',
-        'ipphone'=>'IP Telephone',
-        'l'=>'City',
-        'mail'=>'Email Address',
-        'mailnickname'=>'User ID',
-        'rfc822mailbox'=>'Email Address',
-        'mobile'=>'Mobile Telephone',
-        'mobiletelephonenumber'=>'Mobile Telephone',
-        'name'=>'Full Name',
-        'othertelephone'=>'Other Telephone',
-        'ou'=>'Organizational Unit',
-        'pager'=>'Pager',
-        'pagertelephonenumber'=>'Pager',
-        'physicaldeliveryofficename'=>'Office',
-        'postaladdress'=>'Address',
-        'postalcode'=>'Zip Code',
-        'postofficebox'=>'Post Office Box',
-        'samaccountname'=>'User ID',
-        'serialnumber'=>'Serial Number',
-        'sn'=>'Last Name',
-        'surname'=>'Last Name',
-        'st'=>'State',
-        'stateorprovincename'=>'State',
-        'street'=>'Street',
-        'streetaddress'=>'Street',
-        'telephonenumber'=>'Telephone',
-        'title'=>'Title',
-        'uid'=>'User ID',
-        'url'=>'Other Web Page',
-        'userprincipalname'=>'User ID',
-        'wwwhomepage'=>'Main Web Page',
-        'memberof'=>" Member Of"
+        'buildingname' => 'Building',
+        'c' => 'Country',
+        'cn' => 'Full Name',
+        'co' => 'Country',
+        'comment' => 'Comment',
+        'commonname' => 'Full Name',
+        'company' => 'Company',
+        'description' => 'Description',
+        'distinguishedname' => 'Distinguished Name',
+        'dn' => 'Distinguished Name',
+        'department' => 'Department',
+        'displayname' => 'Full Name',
+        'facsimiletelephonenumber' => 'Facsimile',
+        'fax' => 'Facsimile',
+        'friendlycountryname' => 'Country',
+        'givenname' => 'First Name',
+        'homephone' => 'Home Telephone',
+        'homepostaladdress' => 'Home Address',
+        'info' => 'Information',
+        'initials' => 'Middle Initial',
+        'ipphone' => 'IP Telephone',
+        'l' => 'City',
+        'mail' => 'Email Address',
+        'mailnickname' => 'User ID',
+        'rfc822mailbox' => 'Email Address',
+        'mobile' => 'Mobile Telephone',
+        'mobiletelephonenumber' => 'Mobile Telephone',
+        'name' => 'Full Name',
+        'othertelephone' => 'Other Telephone',
+        'ou' => 'Organizational Unit',
+        'pager' => 'Pager',
+        'pagertelephonenumber' => 'Pager',
+        'physicaldeliveryofficename' => 'Office',
+        'postaladdress' => 'Address',
+        'postalcode' => 'Zip Code',
+        'postofficebox' => 'Post Office Box',
+        'samaccountname' => 'User ID',
+        'serialnumber' => 'Serial Number',
+        'sn' => 'Last Name',
+        'surname' => 'Last Name',
+        'st' => 'State',
+        'stateorprovincename' => 'State',
+        'street' => 'Street',
+        'streetaddress' => 'Street',
+        'telephonenumber' => 'Telephone',
+        'title' => 'Title',
+        'uid' => 'User ID',
+        'url' => 'Other Web Page',
+        'userprincipalname' => 'User ID',
+        'wwwhomepage' => 'Main Web Page',
+        'memberof' => 'Member Of'
     );
 
     /**
@@ -85,55 +85,112 @@ class MainController extends AbstractController
      * @Route("/manage-users", name="manage_users",methods={"GET"})
      */
     public function manage_user(Ldap $ldap, Request $request): Response
-    {   
+    {
 
         $all_entrys = array();
         $attribues_array = array();
         $keys_array = array();
-        
-        $ldap = Ldap::create('ext_ldap', ['host' => '192.168.42.77','port' => '389']);
+
+        $ldap = Ldap::create('ext_ldap', ['host' => '192.168.42.77', 'port' => '389']);
         $search_dn = "cn=admin,dc=akmt,dc=local";
         $search_password = "20Am1299";
         $ldap->bind($search_dn, $search_password);
-        $query = $ldap->query('dc=akmt,dc=local', '(&(objectClass=groupOfNames))');
-        $groups = $query->execute()->toArray();
 
-        foreach ($groups as $group){
-            $dn_groups = $group->getDn();
-            $attributes = array('filter' => array('uid','givenName','sn', 'cn', 'memberof'));
-            $query = $ldap->query('dc=akmt,dc=local', '(&(memberof='.$dn_groups.'))', $attributes  );
-            $usersGroup = $query->execute()->toArray();
+        $attributes = array('filter' => array('uid', 'givenName', 'sn', 'memberof', 'telephonenumber', 'cn'));
+        $query = $ldap->query('dc=akmt,dc=local', '(&(objectclass=person))', $attributes);
+        $usersGroup = $query->execute()->toArray();
 
-            foreach ($usersGroup as $userGroup){
-                $dn_user = $userGroup->getDn();
-                $attribues = $userGroup->getAttributes();
-                
-                foreach($attribues as $keyAttribue => $valueAttribue){
+        foreach ($usersGroup as $userGroup) {
+            $dn_user = $userGroup->getDn();
+            $attribues = $userGroup->getAttributes();
 
-                    if($valueAttribue){
+            foreach ($attribues as $keyAttribue => $valueAttribue) {
+                if ($valueAttribue) {
+                    $array_memberOf = array();
+                    if ($keyAttribue == "memberOf") {
+                        foreach ($valueAttribue as $value) {
+                            array_push($array_memberOf, ldap_explode_dn($value, 1)[0]);
+                        }
+                        $attribues_array[$keyAttribue] = implode("; ", $array_memberOf);
+                    } else {
                         $attribues_array[$keyAttribue] = implode("; ", $valueAttribue);
                     }
                 }
-                $all_entrys[ldap_explode_dn($dn_groups,1)[0]][ldap_explode_dn($dn_user,1)[0]] = $attribues_array;
             }
-
-            $all_entrys[ldap_explode_dn($dn_groups,1)[0]]['count'] = count($usersGroup);
+            $attribues_array['Edit'] = "#";
+            $all_entrys[ldap_explode_dn($dn_user, 1)[0]] = $attribues_array;
         }
 
-        foreach($attribues as $keyAttribue => $valueAttribue){
-            if (array_key_exists(strtolower($keyAttribue) , $this->array_ldap)){
-                array_push($keys_array,$this->array_ldap[strtolower($keyAttribue)]);
+        $all_entrys['count'] = count($usersGroup);
+
+        foreach ($attribues as $keyAttribue => $valueAttribue) {
+            if (array_key_exists(strtolower($keyAttribue), $this->array_ldap)) {
+                array_push($keys_array, $this->array_ldap[strtolower($keyAttribue)]);
             }
         }
+        array_push($keys_array, "Edit");
 
         if ($request->isXmlHttpRequest()) {
-        
-            return new JsonResponse($all_entrys) ;
+
+            return $this->render('main/_table_manage_user.html.twig', ['all_entrys' => $all_entrys, 'attributs_keys' => $keys_array]);
         }
 
-        return $this->render('main/manage_users.html.twig',['all_entrys' => $all_entrys, 'attributs_keys' => $keys_array]);
+        return $this->render('main/manage_users.html.twig', ['all_entrys' => $all_entrys, 'attributs_keys' => $keys_array]);
     }
 
+    /**
+     * @Route("/manage-users", name="users_delete", methods={"DELETE"})
+     */
+    public function deleteUsers(Ldap $ldap, Request $request)
+    {   
+        $users = json_decode($request->getContent(),true);
 
+        if (!$users) {
+            $data = [
+              'status' => 404,
+              'errors' => "User is empty",
+            ];
+            return $this->response($data, 404);
+        }
+
+        $userID = array();
+
+        $ldap = Ldap::create('ext_ldap', ['host' => '192.168.42.77', 'port' => '389']);
+        $search_dn = "cn=admin,dc=akmt,dc=local";
+        $search_password = "20Am1299";
+        $ldap->bind($search_dn, $search_password);
+
+        $entryManager = $ldap->getEntryManager();
+
+        foreach($users as $user){
+                
+            $query = $ldap->query('dc=akmt,dc=local', '(&(objectclass=person)(uid='.$user['User ID'].'))');
+            $result = $query->execute();
+
+            foreach ($result as $entry){
+                $entryManager->remove($entry);
+            }
+        }
+
+        $data = [
+            'status' => 200,
+            'message' => "User(s) deleted successfully",
+        ];
+
+        return $this->response($data);
+    }
+ 
+
+  /**
+   * Returns a JSON response
+   *
+   * @param array $data
+   * @param $status
+   * @param array $headers
+   * @return JsonResponse
+   */
+  public function response($data, $status = 200, $headers = [])
+  {
+    return new JsonResponse($data, $status, $headers);
+  }
 }
-
